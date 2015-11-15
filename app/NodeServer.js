fe80a05cebba70dfe165602d7b361636f9f65552
raminetinati <raminetinati@gmail.com>
2015-11-15 21:58:59 +0000

    Update NodeServer.js

--- app/NodeServer.js
+++ app/NodeServer.js
@@ -96,8 +96,36 @@ var tweetDoc = new mongoose.Schema({
   status: String,
 });
 
+var Message = mongoose.model('Message', tweetDoc); 
+
+
+io.on('connection', function (socket) {
+
+     socket.on('load_data', function (data) {
+        console.log("Loading New Application User");
+        //console.log("emitting filter:", filter); 
+        loadDatabaseData(socket);        
+    });
+
+
+});
 
 
 
+function loadDatabaseData(socket){
+    var response = [];
+    Message.find(function (err, responses) {
+    if (err) return console.error(err);
+     //console.log(responses);
+     try{
+     socket.emit("historic_data", responses.slice((responses.length-5000), (responses.length-1)));
+        }catch(e){
 
+     	socket.emit("historic_data", responses);
+
+
+        }
+    })
+
+}
 
